217ef600c6d6de84a328755ff3f3a003ba2cd989
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 05:28:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+  def initialize
+    @knowledge =[]
+  end
+  def learn(string_knowledge)
+    @knowledge << string_knowledge
+  end
+  def knowledge
+    return @knowledge
+  end
 end
